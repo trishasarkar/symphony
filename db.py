f351f473a1c5ca8d1b05caa5c832de8d1df38591
mohitparekh7 <54593876+mohitparekh7@users.noreply.github.com>
2020-03-11 01:10:17 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,15 @@
+import sqlite3
+c = sqlite3.connect("emotionplayer.db")
+cr=c.cursor()
+def create():
+    cr.execute("CREATE TABLE if not exists song(s_id INTEGER PRIMARY KEY,s_name TEXT )")
+    cr.execute("CREATE TABLE if not exists emotion(e_id INTEGER PRIMARY KEY,e_name TEXT )")
+    cr.execute("CREATE TABLE if not exists album(al_id INTEGER PRIMARY KEY,al_name TEXT )")
+    cr.execute("CREATE TABLE if not exists artist(art_id INTEGER PRIMARY KEY,art_name TEXT )")
+    cr.execute("CREATE TABLE if not exists songart(s_id INTEGER , art_id INTEGER )")
+    cr.execute("CREATE TABLE if not exists songalb(s_id INTEGER , al_id INTEGER )")
+    cr.execute("CREATE TABLE if not exists songemo(s_id INTEGER , e_id INTEGER )")
+    c.commit()
+    cr.close()
+    c.close()
+create()
