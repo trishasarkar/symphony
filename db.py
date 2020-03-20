b0f827c4facd471c462a8ec74d84a4cd1b4fba9d
mohitparekh7 <54593876+mohitparekh7@users.noreply.github.com>
2020-03-20 15:46:49 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,10 @@
 import sqlite3
+
 c = sqlite3.connect("emotionplayer.db")
 cr=c.cursor()
+
 def create():
-    cr.execute("CREATE TABLE if not exists song(s_id INTEGER PRIMARY KEY,s_name TEXT )")
+    cr.execute("CREATE TABLE if not exists song(s_id INTEGER PRIMARY KEY,s_name TEXT,s_lang TEXT )")
     cr.execute("CREATE TABLE if not exists emotion(e_id INTEGER PRIMARY KEY,e_name TEXT )")
     cr.execute("CREATE TABLE if not exists album(al_id INTEGER PRIMARY KEY,al_name TEXT )")
     cr.execute("CREATE TABLE if not exists artist(art_id INTEGER PRIMARY KEY,art_name TEXT )")
@@ -12,4 +14,4 @@ def create():
     c.commit()
     cr.close()
     c.close()
-create()
+#create()
